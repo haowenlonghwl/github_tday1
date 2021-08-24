24a6cd3d9c8563e5c4f7c2307154cb6418565a89
2101student <15512508680@163.com>
2021-08-24 11:16:47 +0800

    这是我们第二次idea集成github

---
+++ src/main/java/cn/baizhi/GithubDay1Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubDay1Application.class, args);
+    }
+
+}
