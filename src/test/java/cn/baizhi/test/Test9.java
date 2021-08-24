c8340e2db35b5d708861ea0aaa992b1edb9550a0
haowenlonghwl <89377898+haowenlonghwl@users.noreply.github.com>
2021-08-24 11:32:28 +0800

    Update Test9.java

--- src/test/java/cn/baizhi/test/Test9.java
+++ src/test/java/cn/baizhi/test/Test9.java
@@ -3,6 +3,9 @@ package cn.baizhi.test;
 public class Test9 {
     public static void main(String[] args){
         System.out.println("来了");
+        
+        int a = 20;
+        int b = 30;
 
 
     }
